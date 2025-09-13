73f7afc46a74fbbeb9aca333edd07e1db94bcc22
JoeMartin2001 <sardorbekaminjonov2001@mail.ru>
2025-09-13 05:59:23 +0500

    Add UserModule to AppModule imports for enhanced user management functionality.

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule } from 'src/infra/config/config.module';
 import { DatabaseModule } from 'src/infra/database/database.module';
 import { GqlModule } from 'src/infra/graphql/graphql.module';
+import { UserModule } from '../user/user.module';
 
 @Module({
-  imports: [ConfigModule, DatabaseModule, GqlModule],
+  imports: [ConfigModule, DatabaseModule, GqlModule, UserModule],
 })
 export class AppModule {}
