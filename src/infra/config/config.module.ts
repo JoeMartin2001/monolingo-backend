6b90ea929da1c10eecbd66dcbf298754c058aa6a
JoeMartin2001 <sardorbekaminjonov2001@mail.ru>
2025-09-13 05:41:14 +0500

    Refactor configuration files by moving app and database configurations to dedicated modules, and add environment validation to ensure required variables are correctly set.

--- src/infra/config/config.module.ts
+++ src/infra/config/config.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule as NestConfigModule } from '@nestjs/config';
-import appConfig from 'src/config/app.config';
-import databaseConfig from 'src/config/database.config';
-import { validate } from 'src/config/env.validation';
+import appConfig from 'src/infra/config/register-app.config';
+import databaseConfig from 'src/infra/config/register-database.config';
+import { validate } from 'src/infra/config/env.validation';
 
 @Module({
   imports: [
